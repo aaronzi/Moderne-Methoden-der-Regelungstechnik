898e597baf8c18e797e334e94018e3544f0e0618
Aaron Zielstorff <aaron.zi@web.de>
2022-06-27 18:34:41 +0200

    added pictures for Reglervalidierung nichtlinear

--- Inverses Pendel/2. Matlab_Simulink/Reglervalidierung.m
+++ Inverses Pendel/2. Matlab_Simulink/Reglervalidierung.m
@@ -1,4 +1,5 @@
-addpath(genpath('./Reglervalidierung/'));
+addpath(genpath('./Reglervalidierung/')); % Befehl muss einmal am Anfang
+% ausgeführt werden!
 %% Ackermann (linear) Phi bei verschiedenen Anfangswinkeln
 linear_ackermann_phi();
 %% Ackermann (linear) xM bei verschiedenen Anfangswinkeln
@@ -7,148 +8,31 @@ linear_ackermann_xM();
 linear_ackermann_u();
 %% Vorsteuerung (linear) Phi bei verschiedenen Anfangswinkeln
 linear_vorsteuerung_phi();
-%% Vorsteuerung xM bei verschiedenen Anfangswinkeln
+%% Vorsteuerung (linear) xM bei verschiedenen Anfangswinkeln
 linear_vorsteuerung_xM();
-%% Vorsteuerung u bei verschiedenen Anfangswinkeln
+%% Vorsteuerung (linear) u bei verschiedenen Anfangswinkeln
 linear_vorsteuerung_u();
-%% I-Regler Phi bei verschiedenen Anfangswinkeln
+%% I-Regler (linear) Phi bei verschiedenen Anfangswinkeln
 linear_i_regler_phi();
-%% I-Regler xM bei verschiedenen Anfangswinkeln
+%% I-Regler (linear) xM bei verschiedenen Anfangswinkeln
 linear_i_regler_xM();
-%% I-Regler u bei verschiedenen Anfangswinkeln
+%% I-Regler (linear) u bei verschiedenen Anfangswinkeln
 linear_i_regler_u();
-%% Ackermann Phi bei verschiedenen Anfangswinkeln
-% sP_Acker = [-4 -4 -4 -4];
-for i=1:10
-    start_deg = i*2.7;
-    simOut = sim('Zustandsregelung_mit_Ackermann_nichtlinear','StartTime','0','StopTime','4','FixedStep','1/1e4');
-    mat_x = simOut.x_vec.Phi.Data;
-    vec_phi = mat_x(:,1)*180/pi;
-    t_vec = simOut.tout;
-    plot(t_vec,vec_phi)
-    hold on
-end
-hold off
-%% Ackermann xM bei verschiedenen Anfangswinkeln
-% sP_Acker = [-4 -4 -4 -4];
-for i=1:10
-    start_deg = i*2.7;
-    simOut = sim('Zustandsregelung_mit_Ackermann_nichtlinear','StartTime','0','StopTime','4','FixedStep','1/1e4');
-    mat_x = simOut.x_vec.xM.Data;
-    vec_xM = mat_x(:,1);
-    t_vec = simOut.tout;
-    plot(t_vec,vec_xM)
-    hold on
-end
-hold off
-%% Ackermann u bei verschiedenen Anfangswinkeln
-% sP_Acker = [-4 -4 -4 -4];
-for i=1:10
-    start_deg = i*2.7;
-    simOut = sim('Zustandsregelung_mit_Ackermann_nichtlinear','StartTime','0','StopTime','4','FixedStep','1/1e4');
-    u = simOut.u.Data;
-    t_vec = simOut.tout;
-    plot(t_vec,u)
-    hold on
-end
-hold off
-%% Vorsteuerung Phi bei verschiedenen Anfangswinkeln
-% sP_Vorsteuerung = [-4.5 -4.5 -4.5 -4.5];
-for i=1:5
-    y_vec = [0.1 0.25 0.5 0.75 0.9];
-    y_ref = y_vec(i);
-    subplot(5,1,i)
-    for j=1:6
-        start_deg = j*4;
-        simOut = sim('Zustandsregelung_mit_Vorsteuerung_nichtlinear','StartTime','0','StopTime','2.5','FixedStep','1/1e4');
-        mat_x = simOut.x_vec.Phi.Data;
-        vec_phi = mat_x(:,1)*180/pi;
-        t_vec = simOut.tout;
-        plot(t_vec,vec_phi)
-        hold on
-    end
-end
-hold off
-%% Vorsteuerung xM bei verschiedenen Anfangswinkeln
-% sP_Vorsteuerung = [-4.5 -4.5 -4.5 -4.5];
-for i=1:5
-    y_vec = [0.1 0.25 0.5 0.75 0.9];
-    y_ref = y_vec(i);
-    subplot(5,1,i)
-    for j=1:6
-        start_deg = j*4;
-        simOut = sim('Zustandsregelung_mit_Vorsteuerung_nichtlinear','StartTime','0','StopTime','2.5','FixedStep','1/1e4');
-        mat_x = simOut.x_vec.xM.Data;
-        vec_xM = mat_x(:,1);
-        t_vec = simOut.tout;
-        plot(t_vec,vec_xM)
-        hold on
-    end
-end
-hold off
-%% Vorsteuerung u bei verschiedenen Anfangswinkeln
-% sP_Vorsteuerung = [-4.5 -4.5 -4.5 -4.5];
-for i=1:5
-    y_vec = [0.1 0.25 0.5 0.75 0.9];
-    y_ref = y_vec(i);
-    subplot(5,1,i)
-    for j=1:6
-        start_deg = j*4;
-        simOut = sim('Zustandsregelung_mit_Vorsteuerung_nichtlinear','StartTime','0','StopTime','2.5','FixedStep','1/1e4');
-        u = simOut.u.Data;
-        t_vec = simOut.tout;
-        plot(t_vec,u)
-        hold on
-    end
-end
-hold off
-%% I-Regler Phi bei verschiedenen Anfangswinkeln
-% sP_Vorsteuerung = [-4.5 -4.5 -4.5 -4.5];
-for i=1:5
-    y_vec = [0.1 0.25 0.5 0.75 0.9];
-    y_ref = y_vec(i);
-    subplot(5,1,i)
-    for j=1:6
-        start_deg = j*4;
-        simOut = sim('Zustandsregelung_mit_I_Regelung_nichtlinear','StartTime','0','StopTime','3.5','FixedStep','1/1e4');
-        mat_x = simOut.x_vec.Phi.Data;
-        vec_phi = mat_x(:,1)*180/pi;
-        t_vec = simOut.tout;
-        plot(t_vec,vec_phi)
-        hold on
-    end
-end
-hold off
-%% I-Regler xM bei verschiedenen Anfangswinkeln
-% sP_Vorsteuerung = [-4.5 -4.5 -4.5 -4.5];
-for i=1:5
-    y_vec = [0.1 0.25 0.5 0.75 0.9];
-    y_ref = y_vec(i);
-    subplot(5,1,i)
-    for j=1:6
-        start_deg = j*4;
-        simOut = sim('Zustandsregelung_mit_I_Regelung_nichtlinear','StartTime','0','StopTime','3.5','FixedStep','1/1e4');
-        mat_x = simOut.x_vec.xM.Data;
-        vec_xM = mat_x(:,1);
-        t_vec = simOut.tout;
-        plot(t_vec,vec_xM)
-        hold on
-    end
-end
-hold off
-%% I-Regler u bei verschiedenen Anfangswinkeln
-% sP_Vorsteuerung = [-4.5 -4.5 -4.5 -4.5];
-for i=1:5
-    y_vec = [0.1 0.25 0.5 0.75 0.9];
-    y_ref = y_vec(i);
-    subplot(5,1,i)
-    for j=1:6
-        start_deg = j*4;
-        simOut = sim('Zustandsregelung_mit_I_Regelung_nichtlinear','StartTime','0','StopTime','3.5','FixedStep','1/1e4');
-        u = simOut.u.Data;
-        t_vec = simOut.tout;
-        plot(t_vec,u)
-        hold on
-    end
-end
-hold off
+%% Ackermann (nicht-linear) Phi bei verschiedenen Anfangswinkeln
+nichtlinear_ackermann_phi();
+%% Ackermann (nicht-linear) xM bei verschiedenen Anfangswinkeln
+nichtlinear_ackermann_xM();
+%% Ackermann (nicht-linear) u bei verschiedenen Anfangswinkeln
+nichtlinear_ackermann_u();
+%% Vorsteuerung (nicht-linear) Phi bei verschiedenen Anfangswinkeln
+nichtlinear_vorsteuerung_phi();
+%% Vorsteuerung (nicht-linear) xM bei verschiedenen Anfangswinkeln
+nichtlinear_vorsteuerung_xM();
+%% Vorsteuerung (nicht-linear) u bei verschiedenen Anfangswinkeln
+nichtlinear_vorsteuerung_u();
+%% I-Regler (nicht-linear) Phi bei verschiedenen Anfangswinkeln
+nichtlinear_i_regler_phi();
+%% I-Regler (nicht-linear) xM bei verschiedenen Anfangswinkeln
+nichtlinear_i_regler_xM();
+%% I-Regler (nicht-linear) u bei verschiedenen Anfangswinkeln
+nichtlinear_i_regler_u();
---
+++ Inverses Pendel/2. Matlab_Simulink/Reglervalidierung/nichtlinear_ackermann_xM.m
@@ -0,0 +1,23 @@
+function nichtlinear_ackermann_xM()
+    warning('off','all')
+    % sP_Acker = [-4 -4 -4 -4];
+    legendString = strings(10,1);
+    for i=1:10
+        assignin('base','start_deg',i*2.7);
+        simOut = sim('Zustandsregelung_mit_Ackermann_nichtlinear','StartTime','0','StopTime','4','FixedStep','1/1e4');
+        mat_x = simOut.x_vec.xM.Data;
+        vec_xM = mat_x(:,1);
+        t_vec = simOut.tout;
+        plot(t_vec,vec_xM)
+        hold on
+        legendString(i) = sprintf('%.1f°',i*2.7);
+    end
+    xlabel('Zeit t [s]','interpreter','latex')
+    ylabel('Position $x_M$ [m]','interpreter','latex')
+    legend(legendString)
+    grid on
+    hold off
+    filename = fullfile('./Reglervalidierung/', 'nichtlinear_ackermann_xM.pdf');
+    exportgraphics(gcf,filename,'ContentType','vector')
+    disp('Successfully created PDF')
+end
